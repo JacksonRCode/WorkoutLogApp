ef10ea381df665c0eb98b9401c6699c4dcc666c9
JacksonRCode <jackdareid@gmail.com>
2026-01-28 15:31:48 -0500

    Finish database init.sql file.

--- backend/db/init.sql
+++ backend/db/init.sql
@@ -15,39 +15,83 @@ CREATE TABLE programs (
     name VARCHAR(100) NOT NULL,
     date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     notes TEXT,
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
 
 CREATE TABLE workouts (
     workout_id SERIAL PRIMARY KEY,
-    user_id INT,
+    user_id INT NOT NULL,
     program_id INT,
     name VARCHAR(100),
     notes TEXT,
-    FOREIGN KEY (user_id) REFERENCES users(user_id),
-    FOREIGN KEY (program_id) REFERENCES programs(program_id)
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (program_id) REFERENCES programs(program_id) ON DELETE SET NULL
 );
 
 CREATE TABLE exercises (
-    exercise_id SERIAL PRIMARY KEY
+    exercise_id SERIAL PRIMARY KEY,
+    user_id INT,
+    name VARCHAR(50),
+    muscle_group VARCHAR(50),
+    notes TEXT,
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
 
 CREATE TABLE workout_exercises (
-    order_index INT
+    PRIMARY KEY (workout_id, exercise_id),
+    workout_id INT,
+    exercise_id INT,
+    order_index INT,
+    target_sets INT,
+    target_reps INT,
+    rest INT,
+    time_flag BOOLEAN DEFAULT FALSE,
+    distance INT,
+    notes TEXT,
+    FOREIGN KEY (workout_id) REFERENCES workouts(workout_id) ON DELETE CASCADE,
+    FOREIGN KEY (exercise_id) REFERENCES exercises(exercise_id) ON DELETE CASCADE
 );
 
 CREATE TABLE workout_completed (
-    workout_completed_id SERIAL PRIMARY KEY
+    workout_completed_id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    workout_id INT,
+    start_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    end_time TIMESTAMP,
+    notes TEXT,
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE, 
+    FOREIGN KEY (workout_id) REFERENCES workouts(workout_id) ON DELETE SET NULL
 );
 
 CREATE TABLE completed_exercises (
-    completed_exercise_id SERIAL PRIMARY KEY
+    completed_exercise_id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    exercise_id INT NOT NULL,
+    workout_completed_ID INT NOT NULL,
+    time_flag BOOLEAN,
+    notes TEXT,
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (exercise_id) REFERENCES exercises(exercise_id) ON DELETE CASCADE,
+    FOREIGN KEY (workout_completed_id) REFERENCES workout_completed(workout_completed_id) ON DELETE CASCADE 
 );
 
 CREATE TABLE completed_sets (
-    completed_set_id SERIAL PRIMARY KEY
+    completed_set_id SERIAL PRIMARY KEY,
+    completed_exercise_id INT NOT NULL,
+    weight FLOAT,
+    reps INT,
+    rpe INT,
+    set_number INT NOT NULL,
+    FOREIGN KEY (completed_exercise_id) REFERENCES completed_exercises(completed_exercise_id) ON DELETE CASCADE
 );
 
 CREATE TABLE user_exercise_stats (
-    calculated_max INT
+    PRIMARY KEY (user_id, exercise_id),
+    user_id INT,
+    exercise_id INT,
+    calculated_max INT,
+    date_last_hit TIMESTAMP,
+    all_time_best INT,
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (exercise_id) REFERENCES exercises(exercise_id)
 );
